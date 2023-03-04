9db26b7c7cf5f37d816c546fe58efefd718b538f
mostafizz fuad <mostafizzfuad@gmail.com>
2023-03-04 20:42:53 +0600

    update code

--- 03_ternary.php
+++ 03_ternary.php
@@ -1,10 +1,26 @@
-<?php 
+<?php
 
 // ternary operator (large between three numbers)
 $num1 = 5000;
 $num2 = 8000;
-$num3 = 7000;
+$num3 = 9000;
 
 $largeNumber = ($num1 > $num2) ? $num1 : (($num2 > $num3) ? $num2 : $num3);
 echo $largeNumber;
 
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+// assignment
+$fee = 18500;    //User's input
+
+$twenty_five_percent = $fee * 25 / 100;  //Total of 25 parcent commission 
+$twenty_percent = $fee * 20 / 100;      //Total of 20 parcent commission 
+$fifteen_percent = $fee * 15 / 100;     //Total of 15 parcent commission 
+$invalid = "Invalid";                  //If tution fee is less than 7000
+
+//Logic
+$commission = ($fee >= 20000) ? ($twenty_five_percent) : (($fee > 10000 && $fee < 20000) ? ($twenty_percent) : (($fee < 10000 && $fee > 7000) ? ($fifteen_percent) : $invalid));
+
+echo $commission;
