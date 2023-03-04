976fce9fc57d35117cb29cdb94a14084c432f00b
mostafizz fuad <mostafizzfuad@gmail.com>
2023-03-05 02:53:15 +0600

    update code

--- 03_ternary.php
+++ 03_ternary.php
@@ -24,3 +24,21 @@ $invalid = "Invalid";                  //If tution fee is less than 7000
 $commission = ($fee >= 20000) ? ($twenty_five_percent) : (($fee > 10000 && $fee < 20000) ? ($twenty_percent) : (($fee < 10000 && $fee > 7000) ? ($fifteen_percent) : $invalid));
 
 echo $commission;
+
+
+echo PHP_EOL;
+echo PHP_EOL;
+
+/////////////////////
+$x=80000;
+
+// $output= ($x>=100)?("Greater than 100"):("Less than 100");
+
+// $output=($x>=100)?(($x>=500)?("Greater than 500"):("Less than 500 but grater than 100")):(($x<=50)?("Less than 50"):("Less than 100 but grater than 50"));
+
+// $output =($x>1000)?("Grater than  1000"):("Less than or equal 1000");
+// ()?(()?(()?():()):()):(()?():(()?():()));
+$output =($x>=1000 )?(($x>=50000)?("Grater than 50000"):("Grater than 1000 but less than 50000")):(($x>=0)?("Grater than zero but less than 1000"):("Negative number"));
+
+
+ echo $output;
